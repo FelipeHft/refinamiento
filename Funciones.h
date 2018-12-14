fa1126c05f90f49e8ea0552ad53797ae65a9b64d
pipeworkout <felipe.hernandezc@utem.cl>
2018-12-14 14:41:53 -0300

    inicializacion

--- Funciones.h
+++ Funciones.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Funciones.h
- * Author: pipeworkout
- *
- * Created on 14 de diciembre de 2018, 11:36
- */
-
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: pipeworkout
- *
- * Created on 14 de diciembre de 2018, 11:35
- */
-
 #include <cstdlib>
 #include "Funciones.h"
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+struct coordenada{
+    float x;
+    float y;
+};
 
+int main(int argc, char** argv) {
+    coordenada a, b, c;
+    a.x = 2; 
+    a.y = 4;
+    b.x = -1;
+    b.y = 1;
+    c.x = 4;
+    c.y = -1;
+    
+    angulos(a, b, c);
     
     return 0;
 }
